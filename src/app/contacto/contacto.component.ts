727f47179535045c87dd6cae723c9b855ea31dc5
Carlos Cabrera Criado <carloscabre_96@hotmail.com>
2024-12-03 10:50:56 +0100

    Actualización del AppComponent

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-contacto',
+  selector: "app-contacto",
+  standalone: true,
   imports: [],
-  templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.scss'
+  templateUrl: "./contacto.component.html",
+  styleUrl: "./contacto.component.scss",
 })
-export class ContactoComponent {
-
-}
+export class ContactoComponent {}
