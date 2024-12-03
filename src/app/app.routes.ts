ab6d020940e865bc445d4a37a3f29637616d41f5
Carlos Cabrera Criado <carloscabre_96@hotmail.com>
2024-12-03 10:45:47 +0100

    Archivo de rutas actualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { InicioComponent } from "./inicio/inicio.component";
+import { ContactoComponent } from "./contacto/contacto.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "inicio", pathMatch: "full" },
+  {
+    path: "inicio",
+    pathMatch: "full",
+    canActivate: [],
+    component: InicioComponent,
+  },
+  {
+    path: "contacto",
+    pathMatch: "full",
+    canActivate: [],
+    component: ContactoComponent,
+  },
+];
