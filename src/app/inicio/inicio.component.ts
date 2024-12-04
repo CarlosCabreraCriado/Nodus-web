81013d28e1dfa21f42a799ef8a0699db45253391
Emilio Diaz <emilioopb96@gmail.com>
2024-12-04 14:31:39 +0000

    commit de contacto pagina inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { ContactoComponent } from "../contacto/contacto.component";
 
 @Component({
   selector: "app-inicio",
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ContactoComponent],
   templateUrl: "./inicio.component.html",
   styleUrl: "./inicio.component.scss",
 })
